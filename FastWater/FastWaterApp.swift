d35f90b88dc2969dbd6838a1a2b1ec998b745f3d
Ammar Rajab <ammar.earth@gmail.com>
2025-07-27 12:11:37 +0400

    Initial Commit

---
+++ FastWater/FastWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastWaterApp.swift
+//  FastWater
+//
+//  Created by Ammar on 27/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FastWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
